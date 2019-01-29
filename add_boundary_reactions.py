b712d9d8d67f648f60cecad1ba5fe71d01226775
mephenor <zajac.thomas1992@gmail.com>
2019-01-29 19:07:38 +0100

    Rewritten boundary definition code to only set boundary metabolites for those that need to be imported/exported

--- add_boundary_reactions.py
+++ add_boundary_reactions.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import libsbml
 from collections import defaultdict
 
@@ -37,50 +38,48 @@ def main():
 
     asansm_libsbml_doc = libsbml.readSBML(asansm_sbml)
     asansm_libsbml = asansm_libsbml_doc.getModel()
-    add_boundary_reactions(asansm_libsbml, net_fluxes["Amino sugar and nucleotide sugar metabolism"])
+    add_boundary_metabolites(asansm_libsbml, net_fluxes["Amino sugar and nucleotide sugar metabolism"])
     libsbml.writeSBMLToFile(asansm_libsbml_doc,
                             "xml/iPAE1146_Amino_sugar_and_nucleotide_sugar_metabolism_fbc_squeezed_with_boundaries.xml")
 
     pm_libsbml_doc = libsbml.readSBML(pm_sbml)
     pm_libsbml = pm_libsbml_doc.getModel()
-    add_boundary_reactions(pm_libsbml, net_fluxes["Pyrimidine metabolism"])
+    add_boundary_metabolites(pm_libsbml, net_fluxes["Pyrimidine metabolism"])
     libsbml.writeSBMLToFile(pm_libsbml_doc, "xml/iPAE1146_Pyrimidine_metabolism_fbc_squeezed_with_boundaries.xml")
 
     lb_libsbml_doc = libsbml.readSBML(lb_sbml)
     lb_libsbml = lb_libsbml_doc.getModel()
-    add_boundary_reactions(lb_libsbml, net_fluxes["Lipopolysaccharide biosynthesis"])
+    add_boundary_metabolites(lb_libsbml, net_fluxes["Lipopolysaccharide biosynthesis"])
     libsbml.writeSBMLToFile(lb_libsbml_doc,
                             "xml/iPAE1146_Lipopolysaccharide_biosynthesis_fbc_squeezed_with_boundaries.xml")
 
-    combined_balance = defaultdict(int)
-    for balance_dict in [net_fluxes["Amino sugar and nucleotide sugar metabolism"],
-                         net_fluxes["Pyrimidine metabolism"],
-                         net_fluxes["Lipopolysaccharide biosynthesis"]]:
-        for key, value in balance_dict.items():
-            combined_balance[key] += value
-
     combined_libsbml_doc = libsbml.readSBML(combined_sbml)
     combined_libsbml = combined_libsbml_doc.getModel()
-    add_boundary_reactions(combined_libsbml, combined_balance)
+    for boundary_reactions in [net_fluxes["Amino sugar and nucleotide sugar metabolism"],
+                               net_fluxes["Pyrimidine metabolism"],
+                               net_fluxes["Lipopolysaccharide biosynthesis"]]:
+        add_boundary_metabolites(combined_libsbml, boundary_reactions)
     libsbml.writeSBMLToFile(combined_libsbml_doc, "xml/iPAE1146_Combined_Subsystems_fbc_squeezed_with_boundaries.xml")
 
 
-def add_boundary_reactions(model, boundary_reactions):
-    species_occurrence = defaultdict(int)
+def add_boundary_metabolites(model, boundary_reactions):
+    reactants = defaultdict(int)
+    products = defaultdict(int)
     for reaction in model.getListOfReactions():
         for reactant in reaction.getListOfReactants():
-            species_occurrence[reactant.getId()] += 1
+            reactants[reactant.getSpecies()] += reactant.getStoichiometry()
         for product in reaction.getListOfProducts():
-            species_occurrence[product.getId()] += 1
+            products[product.getSpecies()] += product.getStoichiometry()
 
-    for reaction in boundary_reactions:
+    for r_id in boundary_reactions:
+        reaction = model.getReaction(r_id)
         for reactant in reaction.getListOfReactants():
-            if not species_occurrence[reactant.getId()] > 1:
-                species = model.getSpecies(reactant.getId())
+            if reactants[reactant.getSpecies()] != products[reactant.getSpecies()]:
+                species = model.getSpecies(reactant.getSpecies())
                 species.setBoundaryCondition(True)
         for product in reaction.getListOfProducts():
-            if not species_occurrence[product.getId()] > 1:
-                species = model.getSpecies(product.getId())
+            if reactants[product.getSpecies()] != products[product.getSpecies()]:
+                species = model.getSpecies(product.getSpecies())
                 species.setBoundaryCondition(True)
 
 
